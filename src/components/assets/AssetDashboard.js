9820f37dcc9acc5e9e79dfecea45a14a3cd4b4de
Chris Philbin <chris@chrisphilbin.net>
2022-01-09 11:34:13 -0500

    Adding card element to display each user asset

--- src/components/assets/AssetDashboard.js
+++ src/components/assets/AssetDashboard.js
@@ -18,10 +18,29 @@ const AssetDashboard = () => {
   if (token) {
     return (
       <>
-        <div>Asset Dashboard for logged in users</div>
-        <div>
+        <div className="grid grid-cols-1 md:grid-cols-2 p-5">
           {userAssets.map((asset) => (
-            <span>{asset.assetName}</span>
+            <div
+              className="rounded overflow-hidden shadow-lg"
+              key={asset.assetName}
+            >
+              <img
+                className="w-full"
+                src="https://firebasestorage.googleapis.com/v0/b/asset-tracker-15d95.appspot.com/o/svs_pb_4000.jpg?alt=media&token=2095415b-7535-406c-b34f-37c2ca22a796"
+                alt="Forest"
+              />
+              <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2">{asset.assetName}</div>
+                <p className="text-gray-700 text-base">{asset.notes}</p>
+              </div>
+              <div className="px-6 pt-4 pb-2">
+                {asset.tags.map((tag) => (
+                  <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                    {tag}
+                  </span>
+                ))}
+              </div>
+            </div>
           ))}
         </div>
       </>
